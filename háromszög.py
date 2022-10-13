117b743ec7c8809b98bfbc1b5fd13934881c8131
Petya0327 <peti200304@gmail.com>
2022-10-13 16:56:31 +0200

    dolgozat

---
+++ háromszög.py
@@ -0,0 +1,3 @@
+keremazoldalt=float(input("Kérem a háromszög oldalának a hosszát: "))
+magassag=float(input("Kérem a háromszög magasságát: "))
+print(f"A háromszög területe:{magassag*keremazoldalt/2} cm2")
